9e2cb1a319c0ebecb7e98844d5cf6f0a3559c903
benv123 <89551265+benv123@users.noreply.github.com>
2021-09-05 15:05:28 -0700

    Fixed main method
    Corrected the 3 .compressFile(file) statements so the file was the correct file path

--- LZW Compression/src/LZWCompressionMain.java
+++ LZW Compression/src/LZWCompressionMain.java
@@ -3,10 +3,10 @@ public class LZWCompressionMain{
 	public static void main(String[] args) throws IOException{
 		LZWCompression compressor=new LZWCompression();
 		System.out.println("lzw-file1.txt: ");
-		compressor.compressFile("lzw-file1.txt: ");
+		compressor.compressFile("lzw-file1.txt");
 		System.out.println("lzw-file2.txt: ");
-		compressor.compressFile("lzw-file2.txt: ");
+		compressor.compressFile("lzw-file2.txt");
 		System.out.println("lzw-file3.txt: ");
-		compressor.compressFile("lzw-file3.txt: ");
+		compressor.compressFile("lzw-file3.txt");
 	}
 }
